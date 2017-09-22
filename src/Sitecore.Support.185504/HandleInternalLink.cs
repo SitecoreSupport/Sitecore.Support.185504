effe87a796a40b0143c1e8003f7817e64c36f0bb
Ivan Sheyenko <ivs@sitecore.net>
2017-09-22 11:07:09 +0300

    original code

---
+++ src/Sitecore.Support.185504/HandleInternalLink.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using Sitecore.Modules.EmailCampaign.Core.Pipelines.GenerateLink;
+using Sitecore.Diagnostics;
+using Sitecore.Links;
+using Sitecore.Modules.EmailCampaign.Core;
+using Sitecore.Modules.EmailCampaign.Core.Links;
+using Sitecore.Sites;
+
+namespace Sitecore.Support.Modules.EmailCampaign.Core.Pipelines.GenerateLink.Hyperlink
+{
+  public class HandleInternalLink: GenerateLinkProcessor
+  {
+    public override void Process(GenerateLinkPipelineArgs args)
+    {
+      Assert.IsNotNull(args, "Arguments can't be null");
+      Assert.IsNotNull(args.Url, "Url link can't be null");
+      if (LinksManager.IsAbsoluteLink(args.Url))
+      {
+        if (args.Url == "link:")
+        {
+          args.AbortPipeline();
+        }
+        return;
+      }
+      if (Regex.IsMatch(args.Url, "^([a-zA-Z0-9+.-]+:)"))
+      {
+        args.AbortPipeline();
+        return;
+      }
+      DynamicLink dynamicLink;
+      if (args.Url.IndexOf("~/link.aspx?", StringComparison.InvariantCulture) >= 0 && DynamicLink.TryParse(args.Url, out dynamicLink))
+      {
+        UrlOptions defaultUrlOptions = LinkManager.GetDefaultUrlOptions();
+        defaultUrlOptions.SiteResolving = true;
+        defaultUrlOptions.Site = SiteContext.GetSite(args.WebsiteConfigurationName);
+        args.Url = LinkManager.GetItemUrl(new ItemUtilExt().GetItem(dynamicLink.ItemId), defaultUrlOptions);
+      }
+      if (args.Url.IndexOf('/') != 0)
+      {
+        args.Url = args.Url.Insert(0, "/");
+      }
+    }
+  }
+}
